bf28c1650c90f313765e48f14471f42f43784ae4
rewa1437 <aarewa1437@gmail.com>
2025-09-04 23:17:15 +0700

    Add boolean datatype example and print statement in main function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
 fn main() {
+    // integer datatype
     // let x:&str = "anupap";
     // let y: i64 = 32;
     // println!("my age is {} {}", y , x);
-    let 
+
+    //float
+    // let pi: f32 = 3.14;
+    // println!("value of pi {}", pi)
+
+    //boolean datatype
+    // let big:bool = true;
+    // println!("my name is big {}", big)
+
+    // charater datatype
+    // let first_letter : char = 'a';
+    // println!("my first letter of my name is {}", first_letter)
+
 }
